0766ef1032126352cc1b1d88292cc7255ba91dca
Shandavine chemutai <chemutaibrenda794@gmail.com>
2026-06-20 17:00:25 +0300

    AquaTrack

---
+++ Backend/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+const db = require("./config/db");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("AquaTrack Backend Running");
+});
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
